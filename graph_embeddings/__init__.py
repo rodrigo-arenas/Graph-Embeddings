2ec6d52fe1516698be7bfdce5fd9fdbb04b5d8cc
rodrigo.arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2022-06-26 11:04:02 -0500

    tests with different graphs

---
+++ graph_embeddings/__init__.py
@@ -0,0 +1,6 @@
+from .algorithms import StackedNode2Vec
+
+from ._version import __version__
+
+
+__all__ = ["StackedNode2Vec", "__version__"]
--- graph_embeddings/algorithms/tests/test_node2vec.py
+++ graph_embeddings/algorithms/tests/test_node2vec.py
@@ -7,7 +7,7 @@ from ..node2vec import StackedNode2Vec
 g1 = nx.DiGraph()
 g2 = nx.DiGraph()
 g1.add_edges_from([("A", "B"), ("B", "C"), ("C", "B"), ("B", "E")])
-g2.add_edges_from([("A", "B"), ("B", "C"), ("C", "B"), ("B", "E")])
+g2.add_edges_from([("A", "B"), ("B", "D"), ("D", "C"), ("C", "D")])
 
 g_nx = [g1, g2]
 g_stellar = [StellarGraph.from_networkx(g) for g in g_nx]
@@ -21,9 +21,9 @@ def test_node2vec_properties(graph):
     embeddings_model = StackedNode2Vec(node_embeddings_size=16)
     embeddings_model.fit(graph)
 
-    assert len(embeddings_model.unique_nodes) == 4
-    assert embeddings_model.embeddings.shape == (4, 16, 2)  # nodes, embedding_size, graphs
-    assert embeddings_model.dense_embeddings.shape == (2, 16 * 4)  # graphs, embedding_size*nodes
+    assert len(embeddings_model.unique_nodes) == 5
+    assert embeddings_model.embeddings.shape == (5, 16, 2)  # nodes, embedding_size, graphs
+    assert embeddings_model.dense_embeddings.shape == (2, 16 * 5)  # graphs, embedding_size*nodes
     assert np.array_equal(embeddings_model.embeddings, embeddings_model.get_embeddings())
     assert np.array_equal(embeddings_model.dense_embeddings, embeddings_model.get_dense_embeddings())
 
